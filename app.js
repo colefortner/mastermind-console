b5ace0df397911159129c7ee0d413f800a058ab6
Cole Fortner <colefortner@gmail.com>
2021-07-16 23:57:50 -0400

    Add code to solve for solution with unique peg colors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const input = require('readline-sync');
 
-const solution = ['red', 'white', 'blue', 'yellow', 'green'];
+const solution = ['red', 'white', 'blue', 'yellow'];
 
 console.log(solution);
 
@@ -15,28 +15,34 @@ while(!gameOver ){
     let wrong = 0;
 
     let guess1 = input.question('What is the first color? ');
-    let guess2 = input.question('Waht is the second color? ');
+    let guess2 = input.question('What is the second color? ');
     let guess3 = input.question('What is the third color? ');
-    let guess4 = input.question('What is the fourth question? ');
-    let guess5 = input.question('What is the fifth color? ');
+    let guess4 = input.question('What is the fourth color? ');
 
-    let guesses = [guess1, guess2, guess3, guess4, guess5];
+    let guesses = [guess1, guess2, guess3, guess4];
+    let accounted = [];
 
     for(let i = 0; i < solution.length; i++){
         if(guesses[i] === solution[i]){
+            accounted.push(i);
             right += 1;
         } else if(!solution.includes(guesses[i])){
+            accounted.push(i)
             wrong += 1;
-        } else {
+        } else if(solution.includes(guesses[i]) && !accounted.includes(solution.indexOf(guesses[i]))) {
+            accounted.push(solution.indexOf(guesses[i]))
             close += 1;
+        } else {
+            wrong += 1;
         }
+        console.log(accounted)
     }
 
     console.log(solution);
     console.log(guesses);
-    console.log(`You got ${right} right`);
-    console.log(`You got ${wrong} completely wrong`);
-    console.log(`You are close on ${close} others`);
+    console.log(`${right} black pegs`);
+    console.log(`${wrong} empty`);
+    console.log(`${close} white pegs`);
 
     if(right === 5){
         gameOver = true;
